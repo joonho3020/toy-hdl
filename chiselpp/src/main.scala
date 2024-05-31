e2a6772b3ad3136ea1f8d259028a81a81dba1909
joonho3020 <joonho.whangbo@berkeley.edu>
2024-05-31 01:50:05 -0700

    Revert "Try out type class derivation, works, but not sure if this integrates well..."
    This reverts commit 9048b7f8d7be18c26ba623de593c9f188452ff46.

--- chiselpp/src/main.scala
+++ chiselpp/src/main.scala
@@ -1,15 +1,7 @@
 package chiselpp
 
-enum HWNode derives CloneType:
-  case AddOp(x: Int)
-  case Literal
-
 object Main:
   def main(args: Array[String]): Unit =
-    val stringnode = summon[CloneType[String]]
-    println(stringnode)
-    val uintnode = summon[CloneType[HWNode]]
-    println(uintnode)
 // println("Hello world")
 // val uint1 = UInt(Width(32))
 // println(s"uint ${uint1}")
@@ -24,15 +16,16 @@ object Main:
 // val reg = Reg(UInt(4.W))
 // println(s"reg ${reg}")
 
-// val mod = Module {
-// val n1 = 0.U(4.W)
-// val n2 = 1.U(4.W)
-// val n3 = n1 + n2
-// val handle = InModuleBody {
-// val n1 = 2.U(4.W)
-// val n2 = 3.U(4.W)
-// val n3 = n1 + n2
-// n3
-// }
-// }
-// println(mod)
+    val mod = Module {
+      val n1 = 0.U(4.W)
+      val n2 = 1.U(4.W)
+      val n3 = n1 + n2
+      val handle = InModuleBody {
+        val n1 = 2.U(4.W)
+        val n2 = 3.U(4.W)
+        val n3 = n1 + n2
+        n3
+      }
+    }
+    println(mod)
+
--- chiselpp/src/nodes.scala
+++ chiselpp/src/nodes.scala
@@ -1,115 +1,62 @@
 package chiselpp
-
 import scala.collection.mutable.ArrayBuffer
-import scala.collection.AbstractIterable
-import scala.compiletime.{erasedValue, error, summonInline}
-import scala.deriving.*
-
-inline def summonInstances[T, Elems <: Tuple]: List[CloneType[?]] =
-  inline erasedValue[Elems] match
-    case _: (elem *: elems) => deriveOrSummon[T, elem] :: summonInstances[T, elems]
-    case _: EmptyTuple => Nil
-
-inline def deriveOrSummon[T, Elem]: CloneType[Elem] =
-  inline erasedValue[Elem] match
-    case _: T => deriveRec[T, Elem]
-    case _    => summonInline[CloneType[Elem]]
-
-inline def deriveRec[T, Elem]: CloneType[Elem] =
-  inline erasedValue[T] match
-    case _: Elem => error("infinite recursive derivation")
-    case _       => CloneType.derived[Elem](using summonInline[Mirror.Of[Elem]]) // recursive derivation
-
-def toTuple(xs: List[_], acc: Tuple): Tuple =
-  xs match
-    case Nil => acc
-    case (h :: t) => h *: toTuple(t, acc)
-
-sealed abstract class CloneType[T]:
-  def cloneType(): T
-
-object CloneType:
-  given CloneType[String] with
-    def cloneType() = new String
-
-  given CloneType[Int] with
-    def cloneType() = 0
-
-  def CloneTypeSum[T](s: Mirror.SumOf[T], elems: => List[CloneType[?]]): CloneType[T] =
-    new CloneType[T]:
-      def cloneType(): T =
-        elems(0).asInstanceOf[CloneType[T]].cloneType()
-
-  def CloneTypeProduct[T](p: Mirror.ProductOf[T], elems: => List[CloneType[?]]): CloneType[T] =
-    new CloneType[T]:
-      def cloneType(): T =
-        p.fromProduct(toTuple(elems.map(_.cloneType()), EmptyTuple))
-
-  inline def derived[T](using m: Mirror.Of[T]): CloneType[T] =
-    lazy val elemInstances = summonInstances[T, m.MirroredElemTypes]
-    inline m match
-      case s: Mirror.SumOf[T]     => CloneTypeSum(s, elemInstances)
-      case p: Mirror.ProductOf[T] => CloneTypeProduct(p, elemInstances)
-
-
-
 
 // For the IR to be a graph rep, we want the elaborated in memory
 // representation of the frontend to be also a graph
-// sealed abstract class HWNode:
-// val nodes = new ArrayBuffer[HWNode]()
-// def add_node(n: HWNode): Unit = nodes += n
-// def cloneType: HWNode
+sealed abstract class HWNode:
+  val nodes = new ArrayBuffer[HWNode]()
+  def add_node(n: HWNode): Unit = nodes += n
+  def cloneType: HWNode
 
-// sealed abstract class HWOp extends HWNode
+sealed abstract class HWOp extends HWNode
 
-// sealed class AddOp extends HWOp:
-// def cloneType: AddOp = new AddOp
+sealed class AddOp extends HWOp:
+  def cloneType: AddOp = new AddOp
 
-// sealed abstract trait HasOperation:
-// this: HWNode =>
+sealed abstract trait HasOperation:
+  this: HWNode =>
 
-// def + [T <: HWNode](b: T)(implicit builder: Module): HWNode =
-// builder.add_node(this)
-// builder.add_node(b)
+  def + [T <: HWNode](b: T)(implicit builder: Module): HWNode =
+    builder.add_node(this)
+    builder.add_node(b)
 
-// val addop = new AddOp
-// builder.add_node(addop)
-// this.add_node(addop)
-// b.add_node(addop)
+    val addop = new AddOp
+    builder.add_node(addop)
+    this.add_node(addop)
+    b.add_node(addop)
 
-// val output = b.cloneType
-// builder.add_node(output)
-// addop.add_node(output)
-// output
+    val output = b.cloneType
+    builder.add_node(output)
+    addop.add_node(output)
+    output
 
-// sealed class Literal[T <: HWType](val value: Int, val hwtype: HWType)
-// extends HWNode with HasOperation:
-// def cloneType: Literal[T] = new Literal(value, hwtype)
-// override def toString: String = s"Literal ${value} ${hwtype}"
+sealed class Literal[T <: HWType](val value: Int, val hwtype: HWType)
+    extends HWNode with HasOperation:
+  def cloneType: Literal[T] = new Literal(value, hwtype)
+  override def toString: String = s"Literal ${value} ${hwtype}"
 
-// sealed class Wire[T <: HWType](val data: HWType) extends HWNode:
-// def cloneType: Wire[T] = new Wire(data)
+sealed class Wire[T <: HWType](val data: HWType) extends HWNode:
+  def cloneType: Wire[T] = new Wire(data)
 
-// object Wire:
-// def apply[T <: HWType](data: HWType) = new Wire(data)
+object Wire:
+  def apply[T <: HWType](data: HWType) = new Wire(data)
 
-// sealed class Reg[T <: HWType](val data: HWType) extends HWNode:
-// def cloneType: Reg[T] = new Reg(data)
+sealed class Reg[T <: HWType](val data: HWType) extends HWNode:
+  def cloneType: Reg[T] = new Reg(data)
 
-// object Reg:
-// def apply[T <: HWType](data: HWType) = new Reg(data)
+object Reg:
+  def apply[T <: HWType](data: HWType) = new Reg(data)
 
-// class Module extends HWNode:
-// def cloneType: Module = new Module
-// override def toString = nodes.map(_.toString).foldLeft("")(_ + "\n" + _)
+class Module extends HWNode:
+  def cloneType: Module = new Module
+  override def toString = nodes.map(_.toString).foldLeft("")(_ + "\n" + _)
 
-// object Module:
-// def apply(init: Module ?=> Unit) =
-// given m: Module = new Module()
-// init
-// m
+object Module:
+  def apply(init: Module ?=> Unit) =
+    given m: Module = new Module()
+    init
+    m
 
-// object InModuleBody:
-// def apply(init: Module ?=> HWNode)(using m: Module) =
-// init
+object InModuleBody:
+  def apply(init: Module ?=> HWNode)(using m: Module) =
+    init
