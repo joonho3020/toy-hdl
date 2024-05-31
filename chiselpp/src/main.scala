e3c457f499e3584cc82c82d4ebd4045fe4111fef
joonho3020 <joonho.whangbo@berkeley.edu>
2024-05-30 18:14:25 -0700

    WIP

--- chiselpp/src/main.scala
+++ chiselpp/src/main.scala
@@ -1,7 +1,7 @@
 package chiselpp
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main:
+  def main(args: Array[String]): Unit =
     println("Hello world")
     val uint1 = UInt(Width(32))
     println(s"uint ${uint1}")
@@ -9,5 +9,16 @@ object Main {
     println(s"uint ${uint2}")
     val uint3 = 4.U(4.W)
     println(s"uint ${uint3}")
-  }
-}
+
+    val wire = Wire(UInt(4.W))
+    println(s"wire ${wire} ${wire.data}")
+
+    val reg = Reg(UInt(4.W))
+    println(s"reg ${reg}")
+
+    val mod = module {
+      val n1 = 0.U(32.W)
+      val n2 = 2.U(4.W)
+      val n3 = n1.add(n2)
+    }
+    println(mod)
--- chiselpp/src/types.scala
+++ chiselpp/src/types.scala
@@ -1,35 +1,67 @@
 package chiselpp
+import scala.collection.mutable.ArrayBuffer
 
-sealed class Width(val value: Int) {
+sealed class Width(val value: Int):
   override def toString: String = s"${value}"
-}
 
-object Width {
+object Width:
   def apply(x: Int): Width = new Width(x)
-}
 
-sealed abstract class Bits(val width: Width) {
+sealed abstract class HWType
+
+// For the IR to be a graph rep, we want the elaborated in memory
+// representation of the frontend to be also a graph
+abstract class HWNode
+
+sealed abstract class Bits(val width: Width) extends HWType:
   override def toString: String = s"Bits ${width}"
-}
 
-sealed class UInt(width: Width) extends Bits(width) {
+sealed class UInt(width: Width) extends Bits(width):
   override def toString: String = s"UInt ${width}"
-}
 
-sealed class UIntLiteral(val value: Int, width: Width) extends UInt(width) {
-  require(value <= ((BigInt(1) << width.value) - 1),
-    f"Value ${value} overflows the number of bits ${width.value}")
+sealed class Literal[T <: HWType](val value: Int, val hwtype: HWType) extends HWNode:
+  override def toString: String = s"Literal ${value} ${hwtype}"
 
-  override def toString: String = s"UIntLiteral ${value} ${width}"
-}
+  def add(b: HWNode)(implicit builder: Module): HWNode =
+    builder.add(this)
+    builder.add(b)
+    this
 
-object UInt {
+object UInt:
   def apply(width: Width): UInt = new UInt(width)
-  def apply(value: Int, width: Width): UIntLiteral = new UIntLiteral(value, width)
-}
+  def apply(value: Int, width: Width): Literal[UInt] = new Literal(value, new UInt(width))
 
+sealed class Clock extends Bits(Width(1))
 
-implicit class IntConversions(value: Int) extends AnyVal {
+object Clock:
+  def apply: Clock = new Clock
+
+sealed class Reset extends Bits(Width(1))
+
+object Reset:
+  def apply: Reset = new Reset
+
+// Add some syntatic sugar using implicit conversions
+implicit class IntConversions(value: Int) extends AnyVal:
   def W: Width = Width(value)
-  def U(w: Width) = UIntLiteral(value, w)
-}
+  def U(w: Width) = Literal(value, UInt(w))
+
+sealed class Wire[T <: HWType](val data: HWType) extends HWNode
+
+object Wire:
+  def apply[T <: HWType](data: HWType) = new Wire(data)
+
+sealed class Reg[T <: HWType](val data: HWType) extends HWNode
+
+object Reg:
+  def apply[T <: HWType](data: HWType) = new Reg(data)
+
+class Module:
+  val nodes = new ArrayBuffer[HWNode]
+  def add(n: HWNode): Unit = nodes += n
+  override def toString = nodes.map(_.toString).foldLeft("")(_ + "\n" + _)
+
+def module(init: Module ?=> Unit) =
+  given m: Module = new Module()
+  init
+  m
