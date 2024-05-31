2c1620c724277de27faa52f0e229aa452e9a72ad
joonho3020 <joonho.whangbo@berkeley.edu>
2024-05-31 13:41:34 -0700

    Revert "?????????????????????????"
    This reverts commit dbb5527af252a467247326f1a4bc11669bd839d6.

--- chiselpp/src/nodes.scala
+++ chiselpp/src/nodes.scala
@@ -3,23 +3,20 @@ import scala.collection.mutable.ArrayBuffer
 
 // For the IR to be a graph rep, we want the elaborated in memory
 // representation of the frontend to be also a graph
-sealed abstract class AbstractHWNode:
-  val nodes = new ArrayBuffer[AbstractHWNode]()
-  def add_node(n: AbstractHWNode): Unit = nodes += n
+sealed abstract class HWNode:
+  val nodes = new ArrayBuffer[HWNode]()
+  def add_node(n: HWNode): Unit = nodes += n
+  def cloneType: HWNode
 
-sealed trait HasType:
-  this: AbstractHWNode =>
-  def hwtype: HWType
+sealed abstract class HWOp extends HWNode
 
-sealed abstract class TypedHWNode extends AbstractHWNode with HasType
-
-sealed abstract class HWOp extends TypedHWNode
-sealed class AddOp extends HWOp
+sealed class AddOp extends HWOp:
+  def cloneType: AddOp = new AddOp
 
 sealed abstract trait HasOperation:
-  this: TypedHWNode =>
+  this: HWNode =>
 
-  def + [T <: TypedHWNode](b: T)(implicit builder: Module): TypedHWNode =
+  def + [T <: HWNode](b: T)(implicit builder: Module): HWNode =
     builder.add_node(this)
     builder.add_node(b)
 
@@ -28,26 +25,30 @@ sealed abstract trait HasOperation:
     this.add_node(addop)
     b.add_node(addop)
 
-    val output = this.hwtype.max(b.hwtype).cloneType
+    val output = b.cloneType
     builder.add_node(output)
     addop.add_node(output)
     output
 
 sealed class Literal[T <: HWType](val value: Int, val hwtype: HWType)
-    extends TypedHWNode with HasOperation:
+    extends HWNode with HasOperation:
+  def cloneType: Literal[T] = new Literal(value, hwtype)
   override def toString: String = s"Literal ${value} ${hwtype}"
 
-sealed class Wire[T <: HWType](val hwtype: HWType) extends TypedHWNode
+sealed class Wire[T <: HWType](val data: HWType) extends HWNode:
+  def cloneType: Wire[T] = new Wire(data)
 
 object Wire:
-  def apply[T <: HWType](hwtype: HWType) = new Wire(hwtype)
+  def apply[T <: HWType](data: HWType) = new Wire(data)
 
-sealed class Reg[T <: HWType](val hwtype: HWType) extends TypedHWNode
+sealed class Reg[T <: HWType](val data: HWType) extends HWNode:
+  def cloneType: Reg[T] = new Reg(data)
 
 object Reg:
-  def apply[T <: HWType](hwtype: HWType) = new Reg(hwtype)
+  def apply[T <: HWType](data: HWType) = new Reg(data)
 
-class Module extends AbstractHWNode:
+class Module extends HWNode:
+  def cloneType: Module = new Module
   override def toString = nodes.map(_.toString).foldLeft("")(_ + "\n" + _)
 
 object Module:
@@ -57,5 +58,5 @@ object Module:
     m
 
 object InModuleBody:
-  def apply(init: Module ?=> AbstractHWNode)(using m: Module) =
+  def apply(init: Module ?=> HWNode)(using m: Module) =
     init
